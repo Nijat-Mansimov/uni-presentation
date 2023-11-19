26f0e98618860b162e2c394321f2eb2fc490f658
Nijat-Mansimov <mansimovnijat@gmail.com>
2023-11-20 02:28:46 +0400

    Added send mail method

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const dotenv = require("dotenv");
+const nodemailer = require('nodemailer');
 
 
 const app = express();
@@ -9,14 +11,30 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', async (req, res) => {
     try {
-        console.log(req.header('x-forwarded-for')) // result "::1"
-        console.log(req.connection.remoteAddress) // result "::1"
-        console.log(req.ip) // result "::1"
+        const transporter = nodemailer.createTransport({
+            host: process.env.SMPT_HOST,
+            port: process.env.SMPT_PORT,
+            service: process.env.SMPT_SERVICE,
+            auth: {
+                user: process.env.SMPT_MAIL,
+                pass: process.env.SMPT_PASSWORD,
+            },
+        });
+
+        const mailOptions = {
+            from: process.env.SMPT_MAIL,
+            to: process.env.SMPT_TO_MAIL,
+            subject: "IP address",
+            text: req.header('x-forwarded-for'),
+        };
+
+        await transporter.sendMail(mailOptions);
+
         res.render('home');
     } catch (error) {
         console.error('Error processing request:', error.message);
